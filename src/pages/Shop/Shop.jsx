e3d780a64ff0a8f1d14e3013643885f259e11602
NurTheDev <nur756.islam@gmail.com>
2025-05-24 00:34:56 +0600

    added comment

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -13,33 +13,34 @@ function Shop() {
     const [view, setView] = React.useState(true);
     const [short, setShort] = React.useState("default");
     const {data, loading, error} = ApiData("../../../../public/furniture_items_api_v4.json");
-    //todo: add sorting and filtering
-    const filteredSortData = useMemo(()=>{
-        if(!data) return [];
+    //todo: add sorting and filtering;
+    // const [sortData, setSortData] = React.useState(data);
+    const filteredSortData = useMemo(() => {
+        if (!data) return [];
         let sortedData = [...data];
         switch (short) {
             case "1":
-                sortedData.sort((a,b)=> {
+                sortedData.sort((a, b) => {
                     return b.sortData.popularity - a.sortData.popularity
                 });
                 break;
             case "2":
-                sortedData.sort((a,b)=> {
+                sortedData.sort((a, b) => {
                     return b.sortData.rating - a.sortData.rating
                 })
                 break;
             case "3":
-                sortedData.sort((a,b)=> {
+                sortedData.sort((a, b) => {
                     return new Date(b.sortData.latestReviewDate) - new Date(a.sortData.latestReviewDate)
                 })
                 break;
             case "4":
-                sortedData.sort((a,b)=> {
+                sortedData.sort((a, b) => {
                     return a.sortData.price - b.sortData.price
                 })
                 break;
             case "5":
-                sortedData.sort((a,b)=> {
+                sortedData.sort((a, b) => {
                     return b.sortData.price - a.sortData.price
                 })
                 break;
@@ -56,30 +57,33 @@ function Shop() {
     const paginatedData = filteredSortData?.slice(startIdx, endIdx);
     return (
         <div>
-            <div className={" bg-[url('../../../src/Images/shopBanner.jpg')] bg-cover bg-no-repeat bg-center lg:py-32 py-16 relative"}>
-               <div className={"w-full h-full bg-white/40 absolute top-0 left-0 flex flex-col items-center justify-center"}>
-                   <h2 className={"heading1"}>Shop</h2>
-                   <p className={"flex items-center"}>Home <MdKeyboardArrowRight/> Shop</p>
-               </div>
+            <div
+                className={" bg-[url('../../../src/Images/shopBanner.jpg')] bg-cover bg-no-repeat bg-center lg:py-32 py-16 relative"}>
+                <div
+                    className={"w-full h-full bg-white/40 absolute top-0 left-0 flex flex-col items-center justify-center"}>
+                    <h2 className={"heading1"}>Shop</h2>
+                    <p className={"flex items-center"}>Home <MdKeyboardArrowRight/> Shop</p>
+                </div>
             </div>
             <div className={"bg-[#F9F1E7]"}>
                 <div
                     className={"container mx-auto px-4 lg:px-6 flex justify-between items-center py-4 lg:py-6 mb-8 lg:mb-16"}>
                     <div className={" gap-x-6 items-center hidden lg:flex"}>
-                        <div className={"flex items-center gap-x-2 cursor-pointer lg:text-xl hover:text-primary "}><span><HiOutlineAdjustmentsHorizontal/></span>
+                        <div className={"flex items-center gap-x-2 cursor-pointer lg:text-xl hover:text-primary "}>
+                            <span><HiOutlineAdjustmentsHorizontal/></span>
                             <span>Filter</span>
                         </div>
-                        <span className={"cursor-pointer lg:text-xl hover:text-primary "} onClick={()=> setView(true)}><HiViewGrid/></span>
-                        <span className={"cursor-pointer lg:text-xl hover:text-primary"} onClick={()=> setView(false)}><BsViewList/></span>
+                        <span className={"cursor-pointer lg:text-xl hover:text-primary "} onClick={() => setView(true)}><HiViewGrid/></span>
+                        <span className={"cursor-pointer lg:text-xl hover:text-primary"} onClick={() => setView(false)}><BsViewList/></span>
                         <p className={"border-l-2 border-l-Gray-4 lg:ps-8 ps-4"}>Showing {showItems} of {data?.length} results</p>
                     </div>
                     <div className={"flex gap-x-6 items-center"}>
                         <div className={"flex items-center gap-x-2 "}>
                             <p className={"lg:text-xl"}>Show</p>
-                            <select onClick={(e)=>{
+                            <select onClick={(e) => {
                                 setShowItems(e.target.value === "1" ? 8 : e.target.value === "2" ? 16 : e.target.value === "3" ? 24 : e.target.value === "4" ? 32 : 40);
                             }}
-                                className={"select select-bordered focus:border-none focus:outline-none select-sm w-full max-w-xs"}>
+                                    className={"select select-bordered focus:border-none focus:outline-none select-sm w-full max-w-xs"}>
                                 <option value="1">8</option>
                                 <option selected>16</option>
                                 <option value="3">24</option>
@@ -89,8 +93,10 @@ function Shop() {
                         </div>
                         <div className={"flex items-center gap-x-2 justify-end"}>
                             <p className={" lg:text-xl lg:w-full text-end"}>Sort by</p>
-                            <select onChange={(e)=>{setShort(e.target.value)}}
-                                className={"select select-bordered select-sm lg:w-full w-1/2 max-w-xs focus:border-none focus:outline-none"}>
+                            <select onChange={(e) => {
+                                setShort(e.target.value)
+                            }}
+                                    className={"select select-bordered select-sm lg:w-full w-1/2 max-w-xs focus:border-none focus:outline-none"}>
                                 <option value="default" selected>Default</option>
                                 <option value="1">Sort by popularity</option>
                                 <option value="2">Sort by average rating</option>
